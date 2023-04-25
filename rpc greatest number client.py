0e2404351e8dac700ad6083a76e7ffd55a13acde
Utkarsh Rana <utkarsh.rana2001@gmail.com>
2023-04-25 22:26:06 +0530

    updated greatest number

--- rpc greatest number client.py
+++ rpc greatest number client.py
@@ -4,9 +4,9 @@ import xmlrpc.client
 proxy = xmlrpc.client.ServerProxy('http://localhost:8000/')
 
 # Define the three numbers
-num1 = 10
-num2 = 25
-num3 = 5
+num1 = int(input("enter 1st number: "))
+num2 = int(input("enter 2nd number: "))
+num3 = int(input("enter 3rd number: "))
 
 # Call the remote function to find the greatest number
 result = proxy.find_greatest(num1, num2, num3)
