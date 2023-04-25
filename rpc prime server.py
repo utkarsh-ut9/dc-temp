5bda53abba7e83694e90c918e5ac59d6250dc8b8
Utkarsh Rana <utkarsh.rana2001@gmail.com>
2023-04-25 21:12:48 +0530

    update prime code2

--- rpc prime server.py
+++ rpc prime server.py
@@ -16,8 +16,5 @@ server = xmlrpc.server.SimpleXMLRPCServer(("localhost", 8000))
 server.register_function(is_prime, "is_prime")
 
 # start the server
-server.serve_forever()
-
-# Start the server
 print('Server listening on port 8000...')
 server.serve_forever()
